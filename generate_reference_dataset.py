4db49ee308fe1bcce893609bea4ddb4d244c4f41
Chris Borg <cborg@citrine.io>
2018-04-04 09:03:05 -0600

    added doc strings, argparse

--- generate_reference_dataset.py
+++ generate_reference_dataset.py
@@ -1,14 +1,25 @@
 import csv
-import sys
+import argparse
 from pypif import pif
 from pypif.obj import *
 
-def parse_csv(csv_file):
+
+def parse_csv(csv_path):
+    """
+    Parses Takeuchi2005 csvs (1, 2a, 2b)
+
+    Args:
+        csv_path (str): path to csv
+
+    Returns:
+        systems (list): list of parsed systems in pif format
+        
+    """
 
     top_row_elements = []
     systems = []
 
-    with open(csv_file, 'r') as f:
+    with open(csv_path, 'r') as f:
 
         reader = csv.reader(f)
 
@@ -19,7 +30,6 @@ def parse_csv(csv_file):
         f.seek(0)
         for index, element in enumerate(top_row_elements):
             if element != "":
-                print("Index: ", index)
                 for csv_index, row in enumerate(reader):
                     if csv_index > 0:
                         if row[index] != "":
@@ -28,15 +38,22 @@ def parse_csv(csv_file):
                             prop = Property(name="Enthalpy of mixing", scalars=row[index], units="kJ/mol")
                             system.properties = [prop]
                             system.references = [Reference(doi="10.2320/matertrans.46.2817")]
-                            print(row[0], element, row[index])
-                            print(pif.dumps(system))
+                            print(pif.dumps(system.chemical_formula), pif.dumps(system.properties))
                             systems.append(system)
             f.seek(0)
+    
+    return systems
 
-    outfile = csv_file.replace(".csv", ".json")
-    pif.dump(systems, open(outfile, "w"))
 
-    print(len(systems))
 if __name__ == "__main__":
 
-    parse_csv(sys.argv[1])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('csv', nargs='*', help='path to csv files')
+
+    args = parser.parse_args()
+
+    for f in args.csv:
+        pifs = parse_csv(f)
+        outfile = f.replace(".csv", ".json")
+        pif.dump(pifs, open(outfile, "w"))
+        print("PIF DUMPED: ", outfile)
